869e9ce7618cf441606bf00250799200fd653fe3
Brayan cadavid <brayan1cadavid@gmail.com>
2024-10-28 21:55:15 -0500

    @feat:disabled csrf token validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            env('APP_URL') . 'api/*',
+            env('APP_URL') . '*',
         ]);
         $middleware->append(Cors::class);
     })
