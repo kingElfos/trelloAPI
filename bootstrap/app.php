ced4a9791280985517c8dbc9c0682fd99034742b
Brayan cadavid <brayan1cadavid@gmail.com>
2024-10-28 21:10:22 -0500

    @fix:added middleware cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->pushMiddleware(Cors::class); // Register Cors middleware
+        $middleware->validateCsrfTokens(except: [
+            env('APP_URL') . '/researchjspost',
+        ]);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
